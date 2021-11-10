30ecb5658114389e25f5c4db7dac47d4cbe37817
DrCash-KMark <markmission10@gmail.com>
2021-11-10 18:59:52 +0100

    Add project files.

---
+++ ExamScheduler/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GeneticSharp;
+
+namespace ExamScheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
