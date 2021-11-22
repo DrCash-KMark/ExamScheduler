11f3c3624bdfe8e383260259b5bc91e01b8973d7
Gergő Szabó <szabo.gergo@edu.bme.hu>
2021-11-22 19:17:20 +0100

    a

---
+++ ExamScheduler/Teacher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamScheduler
+{
+    class Teacher
+    {
+        public String Name { get; set; }
+
+        public bool President { get; set; }
+        public bool Secretary { get; set; }
+        public bool Member { get; set; }
+
+        public List<String> courses;
+
+        public Dictionary<String, bool[]> avability;
+
+        public Teacher()
+        {
+            courses = new List<String>();
+            avability = new Dictionary<String, bool[]>();
+        }
+    }
+}
