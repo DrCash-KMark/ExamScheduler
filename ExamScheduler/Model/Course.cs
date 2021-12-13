37839b650c90ec911c352c003ab1efdacc7cb9c8
Gergő Szabó <szabo.gergo@edu.bme.hu>
2021-12-13 15:50:06 +0100

    model

---
+++ ExamScheduler/Model/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamScheduler
+{
+    class Course
+    {
+        public string name;
+        public string courseCode;
+
+        public Teacher[] teachers;
+    }
+}
---
+++ ExamScheduler/Model/Exam.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamScheduler
+{
+    class Exam
+    {
+        public Student student = null;
+        public Teacher consultant = null;
+        public Teacher examiner = null;
+        public Teacher president = null;
+        public Teacher sercetary = null;
+        public Teacher member = null;
+
+    }
+}
